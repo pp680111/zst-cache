4a8129dc4e21e9d48b644b16cf8f52e291473758
ZST-NOTEBOOK\qq578 <578413616@qq.com>
2024-06-30 10:35:13 +0800

    fix: 补充遗漏的代码

--- src/main/java/com/zst/cache/command/impl/ConfigSeriesCommand.java
+++ src/main/java/com/zst/cache/command/impl/ConfigSeriesCommand.java
@@ -16,7 +16,7 @@ import java.util.List;
  */
 public class ConfigSeriesCommand extends Command {
     @Override
-    protected String getName() {
+    public String getName() {
         return "CONFIG";
     }
 
